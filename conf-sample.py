ea4844823c3488b7d9ad3cc40dd06dc252f04f5b
Hossan Rose <hossan.rose@gmail.com>
2016-10-28 09:39:49 +0530

    README updated

--- conf-sample.py
+++ conf-sample.py
@@ -2,5 +2,5 @@ DEBUG = True
 SECRET_KEY = 'key_value'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:passwd@host:port/db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
 
